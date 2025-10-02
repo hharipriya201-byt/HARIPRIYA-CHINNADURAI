f899716fa47905450b0dc5983b3d02afd4317f32
hharipriya201-byt <hharipriya201@gmail.com>
2025-10-02 10:18:12 +0530

    Delete astronaut_scheduler_project_with_persistence directory

--- astronaut_scheduler_project_with_persistence/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Simple build script
-mkdir -p out
-javac -d out src/main/java/app/*.java
-if [ $? -eq 0 ]; then
-  echo "Build successful. Run with: java -cp out app.Main"
-else
-  echo "Build failed."
-fi
--- astronaut_scheduler_project_with_persistence/src/main/java/app/TaskConflictObserver.java
+++
@@ -1,8 +0,0 @@
-package app;
-
-public interface TaskConflictObserver {
-    void onTaskAdded(Task task);
-    void onTaskConflict(Task newTask, Task conflictingTask);
-    void onTaskRemoved(Task task);
-    void onTaskUpdated(Task task);
-}
